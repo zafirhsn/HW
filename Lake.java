cbec319eeca4cabd0ff6b050dfdd50899046ca67
zafirhsn@gmail.com <zafirhsn@gmail.com>
2015-02-24 09:24:42 -0500

    Progress

--- Lake.java
+++ Lake.java
@@ -5,4 +5,23 @@ public class Lake {
 	{24,20,20,20,20,30},
 	{20,20,14,14,20,20}
     };
+
+    public int stomp(int row, int col, int depth) {
+	int max = 0;
+	for(int i=row;i<=row+2; i++) {
+	    for(int h=col;h<=col+2;h++) {
+		if (pasture[i][h] > max) {
+		    max = pasture[i][h];
+		}
+	    }
+	}
+	return max;
+    }
+
+    public static void main(String[] args) {
+	Lake test = new Lake();
+
+	System.out.println(test.stomp(0,1,4));
+    }
+	    
 }
