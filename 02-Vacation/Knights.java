a8e29279e64830671c87c1014a1d525c3064cb4f
Zafir Hasan <zafirhsn@gmail.com>
2015-03-29 21:12:37 -0400

    Z

--- 02-Vacation/Knights.java
+++ 02-Vacation/Knights.java
@@ -1,3 +1,39 @@
 public class Knights {
-    public Knights() {
+    private char[][] board;
+    private int maxX;
+    private int maxY;
+
+    public void Delay(int n) {
+	try {
+	    Thread.sleep(n);
+	}
+	catch (Exception e){}
     }
+
+    public Knights(int x, int y){
+	board = new char[x][y];
+	
+	for(int i=0;i<x;i++){
+	    for(int j=0;j<y;j++){
+		board[i][j] = '.';
+		Delay(100);
+		System.out.print(board[i][j]);
+	    }
+	    System.out.println();
+	}
+    }
+
+    public Knights() {
+	this(5,5);
+    }
+
+    public void solve() {
+    }
+
+    public static void main(String[] args) {
+	Knights test = new Knights();
+
+    }
+}
+
+    
